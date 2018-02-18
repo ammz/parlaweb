e969f94409ecc4ac239d72c1b90672da3a329486
Antonio Moreno <ammz@deversorius.net>
2018-02-18 19:52:45 +0100

    (f) Diseño con 3 páginas vacias: home, intervenciones, etiquetas

--- parlaweb/views/default.py
+++ parlaweb/views/default.py
@@ -1,33 +1,36 @@
-from pyramid.response import Response
 from pyramid.view import view_config
 
-from sqlalchemy.exc import DBAPIError
+from ..models import Legislatura, Consejeria, Intervencion, Etiqueta
 
-from ..models import MyModel
-
-
-@view_config(route_name='home', renderer='../templates/mytemplate.pt')
-def my_view(request):
-    try:
-        query = request.dbsession.query(MyModel)
-        one = query.filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'one': one, 'project': 'parlaweb'}
+from pyramid.renderers import get_renderer
+from pyramid.interfaces import IBeforeRender
+from pyramid.events import subscriber
 
 
-db_err_msg = """\
-Pyramid is having a problem using your SQL database.  The problem
-might be caused by one of the following things:
+@subscriber(IBeforeRender)
+def globals_factory(event):
+    master = get_renderer('../templates/master.pt').implementation()
+    event['master'] = master
 
-1.  You may need to run the "initialize_parlaweb_db" script
-    to initialize your database tables.  Check your virtual
-    environment's "bin" directory for this script and try to run it.
 
-2.  Your database server may not be running.  Check that the
-    database server referred to by the "sqlalchemy.url" setting in
-    your "development.ini" file is running.
+@view_config(route_name='home', renderer='../templates/principal.pt')
+def selector_view(request):
+    return {
+        'section': 'home',
+        'project': 'parlaweb'}
 
-After you fix the problem, please restart the Pyramid application to
-try it again.
-"""
+
+@view_config(route_name='intervenciones', renderer='../templates/intervenciones.pt')
+def intervenciones_view(request):
+    return {
+        'section': 'intervenciones',
+        'project': 'parlaweb'}
+
+
+@view_config(route_name='etiquetas', renderer='../templates/etiquetas.pt')
+def etiquetas_view(request):
+    return {
+        'section': 'etiquetas',
+        'project': 'parlaweb'}
+
+
