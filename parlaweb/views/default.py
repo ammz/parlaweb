1214b8668b0fef23c5f0d151c8a6d0f94881695b
Antonio Moreno <ammz@deversorius.net>
2018-02-19 17:16:48 +0100

    (f) Listado de intervenciones con sus etiquetas

--- parlaweb/views/default.py
+++ parlaweb/views/default.py
@@ -6,11 +6,20 @@ from pyramid.renderers import get_renderer
 from pyramid.interfaces import IBeforeRender
 from pyramid.events import subscriber
 
+from sqlalchemy import func
+
 
 @subscriber(IBeforeRender)
 def globals_factory(event):
     master = get_renderer('../templates/master.pt').implementation()
     event['master'] = master
+
+
+provincias = ['Almería', 'Cádiz', 'Córdoba', 'Granada', 'Huelva', 'Jaén',
+              'Málaga', 'Sevilla']
+
+link = "http://www.parlamentodeandalucia.es/" +\
+       "webdinamica/portal-web-parlamento/pdf.do?tipodoc=diario&id="
 
 
 @view_config(route_name='home', renderer='../templates/principal.pt')
@@ -22,9 +31,30 @@ def selector_view(request):
 
 @view_config(route_name='intervenciones', renderer='../templates/intervenciones.pt')
 def intervenciones_view(request):
+    """Presenta la lista de intervenciones.
+    """
+    datos_intervenciones = request.dbsession.query(
+        Intervencion,
+        func.string_agg(Etiqueta.nombre,';')
+        ).outerjoin(
+            Intervencion.etiquetas
+        ).order_by(Intervencion.id.desc()).group_by(Intervencion.id).all()
+
+    build_cadena = lambda s: '' if s is None else str(s)
+
+    intervenciones_todas = [dict(
+        id=intervencion.id,
+        fecha=intervencion.fecha.strftime('%d/%m/%Y'),
+        nombre=intervencion.nombre,
+        lista_etiquetas=set(build_cadena(etiquetas).split(";"))-set(provincias),
+        lista_provincias=set(build_cadena(etiquetas).split(";")).intersection(set(provincias)),
+        enlace="%s%s#page=%s" % (link, intervencion.referencia, intervencion.pagina),
+    ) for intervencion, etiquetas in datos_intervenciones]
+
     return {
+        'intervenciones': intervenciones_todas,
         'section': 'intervenciones',
-        'project': 'parlaweb'}
+    }
 
 
 @view_config(route_name='etiquetas', renderer='../templates/etiquetas.pt')
